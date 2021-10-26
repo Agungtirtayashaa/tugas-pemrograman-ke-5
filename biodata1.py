8b0b7d7f247ab29a8e0c4122a3d2aa02b62f1714
agung tirta yasha <agungtirta599@gmail.com>
2021-10-26 22:18:50 +0700

    menambahkan file biodata1.py

---
+++ biodata1.py
@@ -0,0 +1,14 @@
+biodata = [ str("agung tirta yasha"),str("agung"),str("312110561"),str("bekasi"),str("21 tahun"),str("kp rawa gebang rt 02 rw 08 desa tajung baru kec. cikarang timur kab. bekasi"),str("083872903032")]
+
+
+
+print ("    assalamuallaikum    ")
+
+
+
+print("let me introduce my self. my name is",biodata[0],",but you can call me",biodata[1])
+print(".My NPM ",biodata[2],".i was born in ",biodata[3],"and i am ",biodata[4],"years old.")
+print(" i am very glad if you want to invite my house in",biodata[5])
+print("so, don't forget to call me before withe the number",biodata[6],)
+
+print("thanks you")
